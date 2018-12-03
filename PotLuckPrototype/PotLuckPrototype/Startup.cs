fa0eb15e632a850cf2a603372d4d013bdb558196
1760513 <17605139@student.swin.edu.au>
2018-12-03 11:20:08 +1100

    Initial commit

---
+++ PotLuckPrototype/PotLuckPrototype/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PotLuckPrototype.Startup))]
+namespace PotLuckPrototype
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
